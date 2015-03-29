dfb06e71d28a5a2991fada3cfba6c564ba8740b5
Yury <y.timofeyev@gmail.com>
2015-03-29 10:59:05 +0300

    tests for different fail reasons

--- Tests/TeamCityBuildCheckerTests.cs
+++ Tests/TeamCityBuildCheckerTests.cs
@@ -152,5 +152,99 @@ namespace Tests
             Assert.AreEqual(2, _sendMessages.Count);
             Assert.True(_sendMessages[1].Contains("fixed"));
         }
+
+        [Test]
+        public void TeamCityBot_BuildFailedAndStillBrokenDelayPasses_OnlyOneMessagePosted()
+        {
+            builds.Add(new Build() { Id = "1", Number = "1", Status = "FAILED", StatusText = "Tests failed: 25" });
+            var changes = Substitute.For<IChanges>();
+            var change = new Change { Username = "Joe" };
+            changes.ByLocator(null).ReturnsForAnyArgs(new List<Change> { change });
+            tc.Changes.Returns(changes);
+            var checker = new TeamCityBuildChecker(new BuildLocator(), tc, "dev", new TimeConfig { StillBrokenDelay = TimeSpan.FromMilliseconds(10) });
+            var now = new DateTime();
+            DateTimeProvider.SetFunc(() => now);
+
+            checker.CheckBuild(_sendMessageAction);
+            now = now.AddMilliseconds(15);
+            checker.CheckBuild(_sendMessageAction);
+
+            Assert.AreEqual(1, _sendMessages.Count);
+            Assert.True(_sendMessages[0].Contains("is broken"));
+        }
+
+        [Test]
+        public void TeamCityBot_BuildFailedTwoTimesWithDifferentReasons_TwoMessagesPosted()
+        {
+            builds.Add(new Build() { Id = "1", Number = "1", Status = "FAILED", StatusText = "Tests failed: 25" });
+            builds.Add(new Build() { Id = "2", Number = "2", Status = "FAILED", StatusText = "Build failed" });
+            var changes = Substitute.For<IChanges>();
+            var change = new Change { Username = "Joe" };
+            changes.ByLocator(null).ReturnsForAnyArgs(new List<Change> { change });
+            tc.Changes.Returns(changes);
+            var checker = new TeamCityBuildChecker(new BuildLocator(), tc, "dev", new TimeConfig { StillBrokenDelay = TimeSpan.FromMilliseconds(10) });
+            var now = new DateTime();
+            DateTimeProvider.SetFunc(() => now);
+
+            checker.CheckBuild(_sendMessageAction);
+            now = now.AddMilliseconds(5);
+            checker.CheckBuild(_sendMessageAction);
+
+            Assert.AreEqual(2, _sendMessages.Count);
+            Assert.True(_sendMessages[1].Contains("is still broken"));
+        }
+
+        [Test]
+        public void TeamCityBot_BuildFailedTwoTimesBecauseOfNewTests_TwoMessagesPosted()
+        {
+            builds.Add(new Build() { Id = "1", Number = "1", Status = "FAILED", StatusText = "Tests failed: 2" });
+            builds.Add(new Build() { Id = "2", Number = "2", Status = "FAILED", StatusText = "Tests failed: 3" });
+            var changes = Substitute.For<IChanges>();
+            var change = new Change { Username = "Joe" };
+            changes.ByLocator(null).ReturnsForAnyArgs(new List<Change> { change });
+            tc.Changes.Returns(changes);
+            testOccurrences.Add("1", 
+                new TestOccurrence() { Name = "Test: 1"},
+                new TestOccurrence() { Name = "Test: 2" });
+            testOccurrences.Add("2",
+                new TestOccurrence() { Name = "Test: 1" },
+                new TestOccurrence() { Name = "Test: 2" },
+                new TestOccurrence() { Name = "Test: 3" });
+            var checker = new TeamCityBuildChecker(new BuildLocator(), tc, "dev", new TimeConfig { StillBrokenDelay = TimeSpan.FromMilliseconds(10) });
+            var now = new DateTime();
+            DateTimeProvider.SetFunc(() => now);
+
+            checker.CheckBuild(_sendMessageAction);
+            checker.CheckBuild(_sendMessageAction);
+
+            Assert.AreEqual(2, _sendMessages.Count);
+            Assert.True(_sendMessages[1].Contains("is still broken"));
+        }
+
+        [Test]
+        public void TeamCityBot_BuildFailedTwoTimesBecauseOfSameNumberOfDifferentTests_TwoMessagesPosted()
+        {
+            builds.Add(new Build() { Id = "1", Number = "1", Status = "FAILED", StatusText = "Tests failed: 2" });
+            builds.Add(new Build() { Id = "2", Number = "2", Status = "FAILED", StatusText = "Tests failed: 2" });
+            var changes = Substitute.For<IChanges>();
+            var change = new Change { Username = "Joe" };
+            changes.ByLocator(null).ReturnsForAnyArgs(new List<Change> { change });
+            tc.Changes.Returns(changes);
+            testOccurrences.Add("1",
+                new TestOccurrence() { Name = "Test: 1" },
+                new TestOccurrence() { Name = "Test: 2" });
+            testOccurrences.Add("2",
+                new TestOccurrence() { Name = "Test: 1" },
+                new TestOccurrence() { Name = "Test: 3" });
+            var checker = new TeamCityBuildChecker(new BuildLocator(), tc, "dev", new TimeConfig { StillBrokenDelay = TimeSpan.FromMilliseconds(10) });
+            var now = new DateTime();
+            DateTimeProvider.SetFunc(() => now);
+
+            checker.CheckBuild(_sendMessageAction);
+            checker.CheckBuild(_sendMessageAction);
+
+            Assert.AreEqual(2, _sendMessages.Count);
+            Assert.True(_sendMessages[1].Contains("is still broken"));
+        }
     }
 }
